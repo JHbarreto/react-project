0dadcb6a8b74e011d60686ae98574a11151b116f
EvellynRamalho <evellynramalho43@gmail.com>
2025-10-02 20:32:14 -0300

    aprendendo sobre react

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [likes, setLikes] = useState(0);
+  const [input, setInput] = useState('');
+  const [ultimaHora, setUltimaHora] = useState('');
+  
+  function darLike(){
+    setLikes(prevLikes => prevLikes +1);
+    setUltimaHora(new Date().toLocaleTimeString());
+  }
+
+  function resetar(){
+    setInput("");
+    setLikes(0);
+    setUltimaHora("");
+  }
+
+  useEffect(()=>{
+    document.title  = `digitado: ${input} - Likes ${likes}`;
+  }, [input,likes]
+  );
+
+  return (
+    <>
+      <div>Numero de caracteres: {input.length}</div>
+      você digitou:{input}<br></br>
+      <input type="text" value={input} onChange={event => setInput(event.target.value)} placeholder='Digite algo!'/>
+      <div style={{color : likes >= 4  ? 'red' : 'black'}}>Likes: {likes}</div>
+      <button onClick={darLike} disabled={likes >= 4}>
+        {likes>= 4 ? "Limite esgotado" : "Clicar"}</button>
+      <button onClick={resetar} style={{marginLeft:'10px'}}>Resetar</button>
+
+        {likes >= 4 && <p>Você atingiu o limite!</p>}
+
+      <p>Ultimo clique: {ultimaHora || "Nenhuma ainda"}</p>
+    </>
+  )
+}
+
+export default App
